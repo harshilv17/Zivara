fe819cd3a3b53f4b32531ed9899192c41e9abe15
harshilv17 <harshilvalecha@gmail.com>
2026-05-05 20:38:07 +0530

    fix(terraform): update outputs to use ECR data sources

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,11 +3,11 @@ output "s3_bucket_name" {
 }
 
 output "ecr_repository_url_backend" {
-  value = aws_ecr_repository.backend.repository_url
+  value = data.aws_ecr_repository.backend.repository_url
 }
 
 output "ecr_repository_url_frontend" {
-  value = aws_ecr_repository.frontend.repository_url
+  value = data.aws_ecr_repository.frontend.repository_url
 }
 
 output "ecs_cluster_name" {
